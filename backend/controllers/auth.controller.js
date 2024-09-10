e38c0bd27bf213937f5bc319549e6de0889cf61f
William Cheuk Hei Wong <williamwongchurkhay@icloud.com>
2024-09-10 16:57:42 -0400

    Update middleware

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,12 @@
 export async function signup(req, res){
-    res.send("Signup route");
+    try {
+        const {email, password, username} = req.body;
+        if (!email || ! password || !username){
+            return res.status(400).json({success:false, message:"All fields are required"})
+        }
+    } catch (error) {
+        
+    }
 }
 
 export async function login(req, res){
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
-
-dotenv.config();
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
+const PORT = ENV_VARS.PORT;
+
+app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
 
-app.listen(5001, ()=>{
-    console.log('Server started at http://localhost:5001');
-})
+app.listen(PORT, ()=>{
+    console.log('Server started at http://localhost:' + PORT);
+    connectDB();
+}) 
